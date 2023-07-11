0666fe279daf5cc6647d55358abe03cc7170d9aa
mathgenius1991712 <mathgenius1991712@gmail.com>
2023-07-11 17:23:54 +0200

    complete urls

---
+++ iRacingMonitor/mainapp/urls.py
@@ -0,0 +1,20 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('/', views.login, name='index'),
+    path('/login', views.login, name='login'),
+    path('/members', views.members, name='members'),
+    path('/members/add', views.add_member, name='add_member'),
+    path('/members/edit', views.edit_member, name='edit_member'),
+    path('/members/delete', views.delete_member, name='delete_member'),
+    path('/stats', views.stats, name='stats'),
+    path('/stats/export', views.export_stats, name='export_stats'),
+    path('/meta', views.meta, name='meta'),
+    path('/meta/change', views.change_meta, name='change_meta'),
+]
+
+
+
--- iRacingMonitor/mainapp/views.py
+++ iRacingMonitor/mainapp/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#login
+def login(request):
+  print("login")
+
+#members
+def members(request):
+  print("members")
+
+#add_member
+def add_member(request):
+  print("add_member")
+
+#edit_member
+def edit_member(request):
+  print("edit_member")
+
+#delete_member
+def delete_member(request):
+  print("delete_member")
+
+#stats
+def stats(request):
+  print("stats")
+
+#export_stats
+def export_stats(request):
+  print("export_stats")
+
+#meta
+def meta(request):
+  print("meta")
+
+#change_meta
+def change_meta(request):
+  print("change_meta")
+
+
+  
